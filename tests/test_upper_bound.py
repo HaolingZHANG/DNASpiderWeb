eba7194c107d040f12b69f41d784cfc25cfcfefe
HorusZhang <hlzchn@gmail.com>
2021-08-11 15:42:16 +0800

    add tests
    maximum eigenvalue created by scipy package

--- tests/test_upper_bound.py
+++ tests/test_upper_bound.py
@@ -3,6 +3,7 @@ __author__ = "Zhang, Haoling [hlzchn@gmail.com]"
 
 import math
 import numpy
+from scipy.linalg import eig
 from unittest import TestCase
 from dsw import n_system
 from dsw.generator import to_graph
@@ -32,8 +33,14 @@ class TestTerminals(TestCase):
         self.empty_graph = numpy.zeros(shape=(16, 16), dtype=numpy.int)
 
     def test(self):
-        self.assertEqual(approximate_upper_bound(to_graph(self.freed_graph)), 1.0)
-        self.assertEqual(approximate_upper_bound(to_graph(self.empty_graph)), 0.0)
+        freed_maximum_eigenvalue = numpy.real(numpy.max(eig(self.freed_graph)[0]))
+        empty_maximum_eigenvalue = numpy.real(numpy.max(eig(self.empty_graph)[0]))
+        freed_value = math.log(freed_maximum_eigenvalue, len(n_system)) if freed_maximum_eigenvalue > 0.0 else 0.0
+        empty_value = math.log(empty_maximum_eigenvalue, len(n_system)) if empty_maximum_eigenvalue > 0.0 else 0.0
+        self.assertEqual(approximate_upper_bound(to_graph(self.freed_graph)), freed_value)
+        self.assertEqual(approximate_upper_bound(to_graph(self.empty_graph)), empty_value)
+        self.assertEqual(1.0, freed_value)
+        self.assertEqual(0.0, empty_value)
 
 
 class TestBiochemicals(TestCase):
@@ -75,8 +82,14 @@ class TestBiochemicals(TestCase):
                                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
     def test(self):
-        self.assertEqual(approximate_upper_bound(to_graph(self.homopolymer_graph)), math.log(3.0, len(n_system)))
-        self.assertEqual(approximate_upper_bound(to_graph(self.gc_balanced_graph)), math.log(2.0, len(n_system)))
+        h_maximum_eigenvalue = numpy.real(numpy.max(eig(self.homopolymer_graph)[0]))
+        g_maximum_eigenvalue = numpy.real(numpy.max(eig(self.gc_balanced_graph)[0]))
+        h_value = math.log(h_maximum_eigenvalue, len(n_system)) if h_maximum_eigenvalue > 0.0 else 0.0
+        g_value = math.log(g_maximum_eigenvalue, len(n_system)) if g_maximum_eigenvalue > 0.0 else 0.0
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.homopolymer_graph)) - h_value) <= 1e-5, True)
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.gc_balanced_graph)) - g_value) <= 1e-5, True)
+        self.assertEqual(abs(math.log(3.0, len(n_system)) - h_value) <= 1e-5, True)
+        self.assertEqual(abs(math.log(2.0, len(n_system)) - g_value) <= 1e-5, True)
 
 
 class TestCycles(TestCase):
@@ -152,7 +165,84 @@ class TestCycles(TestCase):
                                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
     def test(self):
-        self.assertEqual(approximate_upper_bound(to_graph(self.cycle_2_graph)), 0.0)
-        self.assertEqual(approximate_upper_bound(to_graph(self.cycle_3_graph)), 0.0)
-        self.assertEqual(approximate_upper_bound(to_graph(self.cycle_4_graph)), 0.0)
-        self.assertEqual(approximate_upper_bound(to_graph(self.cycle_5_graph)), 0.0)
+        c_2_maximum_eigenvalue = numpy.real(numpy.max(eig(self.cycle_2_graph)[0]))
+        c_3_maximum_eigenvalue = numpy.real(numpy.max(eig(self.cycle_3_graph)[0]))
+        c_4_maximum_eigenvalue = numpy.real(numpy.max(eig(self.cycle_4_graph)[0]))
+        c_5_maximum_eigenvalue = numpy.real(numpy.max(eig(self.cycle_5_graph)[0]))
+        c_2_value = math.log(c_2_maximum_eigenvalue, len(n_system)) if c_2_maximum_eigenvalue > 0.0 else 0.0
+        c_3_value = math.log(c_3_maximum_eigenvalue, len(n_system)) if c_3_maximum_eigenvalue > 0.0 else 0.0
+        c_4_value = math.log(c_4_maximum_eigenvalue, len(n_system)) if c_4_maximum_eigenvalue > 0.0 else 0.0
+        c_5_value = math.log(c_5_maximum_eigenvalue, len(n_system)) if c_5_maximum_eigenvalue > 0.0 else 0.0
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.cycle_2_graph)) - c_2_value) <= 1e-5, True)
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.cycle_3_graph)) - c_3_value) <= 1e-5, True)
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.cycle_4_graph)) - c_4_value) <= 1e-5, True)
+        self.assertEqual(abs(approximate_upper_bound(to_graph(self.cycle_5_graph)) - c_5_value) <= 1e-5, True)
+        self.assertEqual(abs(c_2_value - 0.0) <= 1e-5, True)
+        self.assertEqual(abs(c_3_value - 0.0) <= 1e-5, True)
+        self.assertEqual(abs(c_4_value - 0.0) <= 1e-5, True)
+        self.assertEqual(abs(c_5_value - 0.0) <= 1e-5, True)
+
+
+class TestSpecials(TestCase):
+
+    def setUp(self):
+        self.graph_1 = numpy.array([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                                    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                                    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0]])
+
+    def test(self):
+        calculated_1_eigenvalue = numpy.real(numpy.max(eig(self.graph_1)[0]))
+        calculated_1_value = math.log(calculated_1_eigenvalue, len(n_system)) if calculated_1_eigenvalue > 0.0 else 0.0
+        approximate_value = approximate_upper_bound(to_graph(self.graph_1))
+        self.assertEqual(abs(approximate_value - calculated_1_value) <= 1e-5, True)
+
+
+class TestRandom(TestCase):
+
+    def setUp(self):
+        self.freed_graph = numpy.array([[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                                        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                                        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                                        [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]])
+        numpy.random.seed(2021)
+        self.test_times = 100
+
+    def test(self):
+        success = 0
+        for time in range(self.test_times):
+            matrix = self.freed_graph.copy()
+            for position in numpy.array(list(numpy.where(self.freed_graph == 1)[:2])).T:
+                if numpy.random.random(1)[0] <= 0.5:
+                    matrix[position[0], position[1]] = 0
+            calculated_eigenvalue = numpy.max(eig(matrix)[0])
+            calculated_value = math.log(calculated_eigenvalue, len(n_system)) if calculated_eigenvalue > 0.0 else 0.0
+            approximate_value = approximate_upper_bound(to_graph(matrix))
+            if abs(approximate_value - calculated_value) <= 1e-3:
+                success += 1
+        # 86
+        self.assertEqual(success / self.test_times >= 0.8, True)
